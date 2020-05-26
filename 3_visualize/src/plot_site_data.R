c95eab279093cc9649de078ba2f40f6ea077dc4d
Alison Appling <aappling@usgs.gov>
2020-05-26 09:59:34 -0400

    fix up steps 2 and 3

--- 3_visualize/src/plot_site_data.R
+++ 3_visualize/src/plot_site_data.R
@@ -1,10 +1,10 @@
-# plot each site indiviudally
-plot_site_data <- function(site_data, out_file) {
-  p <- ggplot(oldest_site_data, aes(x=Date, y=Value, color=Quality)) + geom_line() + ggtitle(Site)
-  ggsave(out_file, plot=p)
+# Plot a timeseries of data for each site individually
+plot_site_data <- function(out_file, site_data, parameter) {
+  p <- ggplot(
+    filter(site_data, Quality %in% c('A','P')), aes(x=Date, y=Value, color=Quality)) +
+    geom_line() +
+    geom_point(data=filter(site_data, !(Quality %in% c('A','P'))), size=0.1) +
+    ylab(dataRetrieval::parameterCdFile %>% filter(parameter_cd == parameter) %>% pull(parameter_nm)) +
+    ggtitle(site_data$Site[1])
+  ggsave(out_file, plot=p, width=6, height=3)
 }
-# plot_sites_data <- function(sites_data, out_dir) {
-#   for(site in unique(sites_data$site_no)) {
-#     site_data <- filter(sites_data, site_no == site)
-#   }
-# }
