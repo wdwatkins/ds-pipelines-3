cfa4de9f0a873fd133f6c4a0ce9cf8e0b79981f4
Alison Appling <aappling@usgs.gov>
2020-06-01 11:16:21 -0400

    move appliers into course

--- 2_process/src/tally_site_obs.R
+++
@@ -1,9 +0,0 @@
-# Compute number of observations per year
-tally_site_obs <- function(site_data) {
-  site_data %>%
-    mutate(Year = lubridate::year(Date)) %>%
-    # group by Site and State just to retain those columns, since we're already
-    # only looking at just one site worth of data
-    group_by(Site, State, Year) %>%
-    summarize(NumObs = length(which(!is.na(Value))))
-}
