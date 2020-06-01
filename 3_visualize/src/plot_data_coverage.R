e07e9c446e08cc4a7f243528612baee2c1bd6838
Alison Appling <aappling@usgs.gov>
2020-06-01 10:42:50 -0400

    prep for createFile actions

--- 3_visualize/src/plot_data_coverage.R
+++
@@ -1,9 +0,0 @@
-plot_data_coverage <- function(oldest_site_tallies, out_file) {
-  oldest_site_tallies %>%
-    mutate(NumObsBin = cut(NumObs, breaks=c(0,100,200,364,366))) %>%
-    ggplot(aes(x=Year, y=sprintf('%s-%s', State, Site), fill=NumObsBin)) + geom_tile() +
-    ylab('State-Site') +
-    ggtitle('Observation counts for champion %s sites', tolower(oldest_site_tallies$Parameter[1])) +
-    scale_fill_brewer('Obs per Year') + theme_bw() -> p
-  ggsave(out_file, plot=p, width=8, height=10)
-}
